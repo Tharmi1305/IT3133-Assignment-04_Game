d9f676b083f40c54174616a70a7629336b0be6cc
Tharmi1305 <139675003+Tharmi1305@users.noreply.github.com>
2024-11-23 18:35:01 +0000

    App.js file modified

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,50 +1,12 @@
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Result from './components/Result';
-import Name from './components/AnimalName'
-import Select from './components/SelectAnimal'
-import { animals } from './assets/data/AnimalsDb';
-import { useState } from 'react';
+
+import React from "react";
+import Gamecom from "./components/Gamecom";
 
 function App() {
-  const [currentAnimal, setCurrentAnimal]=useState(
-    animals[Math.floor(Math.random()* animals.length)]
-  )
-  const [result,setResult]=useState("")
-  
-  const selection = (selectedAnimal)=>{
-    if(selectedAnimal===currentAnimal.name){
-      setResult("Win")
-    }else{
-      setResult("Lose")
-    }
-  }
   return (
-    <div className="App">
-      <br/>
-      <h1><span className='component-border'>Animal Matching Game </span></h1><br/>
-      <Container>
-        <Row>
-          <Col xs={3}>
-            <div className='component-border'>
-              <Result result={result}/>
-            </div>
-          </Col>
-          <Col xs={3}>
-            <div className='component-border'>
-              <Name name={currentAnimal.name}/>
-            </div>
-          </Col>
-          <Col>
-            <div className='component-border'>
-              <Select animals={animals} selection={selection}/>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <>
+      <Gamecom></Gamecom>
+    </>
   );
 }
 
